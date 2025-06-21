6a4df0a27ccb08bcad39804cac5351a8eff3fc98
revcito vampirito <revmithdel@gmail.com>
2025-06-20 21:19:43 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    </head>
+<body>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const welcomeMessage = "¡Bienvenido a EPIC - Home Page!";
+            alert(welcomeMessage);
+            console.log(welcomeMessage);
+        });
+    </script>
+</body>
+</html>
